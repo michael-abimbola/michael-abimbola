4b0e32f1a3c8a2c43ff6388991d683ba8394cf29
1433436 <i433436@fhict.nl>
2021-12-01 18:46:34 +0100

    All projects

---
+++ Jarvis_Project_C#/RandomResponses/RandomResponses/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace RandomResponses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(483, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(492, 269);
+            this.panel1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 313);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "6";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Jarvis_Project_C#/RandomResponses/RandomResponses/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace RandomResponses
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        bool wake = true;
+        Choices choices = new Choices();
+        string name;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+            choices.Add(new string[] { "hello jarvis", "how are you doing jarvis",
+                "jarvis what is todays date", "jarvis what is the time", 
+                "jarvis open google chrome", "jarvis open whatsapp", "jarvis open sublime",
+                "jarvis", "jarvis sleep mode", "jarvis shutdown", "jarvis restart system"});
+            Grammar grm = new Grammar(new GrammarBuilder(choices));
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(grm);
+                sre.SpeechRecognized += Sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+
+            }
+            s.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult, 0, CultureInfo.GetCultureInfo("en-gb"));
+
+        }
+
+        public void jarvis(string jarvisReply)
+        {
+            s.Speak(jarvisReply);
+        }
+
+        public void restart()
+        {
+            //Process.Start(@"M:\Personal projects\RandomResponses\RandomResponses\bin\Debug\RandomResponses.exe");
+            jarvis($"System restarting");
+        }
+        private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string command = e.Result.Text;
+            if (name != "" && name == "Michael")
+            {
+                if (command == "jarvis")
+                {
+                    wake = true;
+                    jarvis($"Hello sir");
+                }
+                else
+                {
+                    if (command == "jarvis sleep mode")
+                    {
+                        wake = false;
+                        jarvis($"See you later {name}" );
+                    }
+                }
+
+                if (wake == true)
+                {
+                    if(command == "jarvis restart system")
+                    {
+                        this.Hide();
+                        restart();                       
+                    }
+                    if (command == "hello jarvis")
+                    {
+                        jarvis($"Welcome back Sir");
+                    }
+                    else
+                    {
+                        if (command == "how are you doing jarvis")
+                        {
+                            jarvis($"I am doing great Sir, thanks for asking");
+                        }
+                        else
+                        {
+                            if (command == "jarvis what is todays date")
+                            {
+                                jarvis($"Today's date is " + DateTime.Now.ToString("M/d/yyy"));
+                            }
+                            else
+                            {
+                                if (command == "jarvis what is the time")
+                                {
+                                    jarvis($"The current time is " + DateTime.Now.ToString("h:mm tt"));
+                                }
+                                else
+                                {
+                                    if (command == "jarvis open google chrome")
+                                    {
+                                        Process.Start("https://www.google.com/");
+                                    }
+                                    else
+                                    {
+                                        if (command == "jarvis open whatsapp")
+                                        {
+                                            Process.Start("https://web.whatsapp.com/");
+                                        }
+                                        else
+                                        {
+                                            //doesnt work for now
+                                            if (command == "jarvis open sublime")
+                                            {
+                                                Process.Start(@"C:\Program Files\Sublime Text 3\sublime_text");
+                                            }
+                                            else
+                                            {
+                                                if (command == "jarvis shutdown")
+                                                {
+                                                    jarvis($"Good day sir");
+                                                    this.Hide();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                jarvis($"would you mind inserting your name sir");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox2.Text;
+        }
+    }
+}
